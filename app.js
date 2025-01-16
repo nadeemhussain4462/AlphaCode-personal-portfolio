61faa121965a62f3766718512dbc5c7af5adcbef
Nadeem Hussain <hoteleyenadeem@gmail.com>
2025-01-16 10:11:30 +0500

    portfolio update

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-// Handle form submission and show popup
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  // Clear the form after submission
-  document.getElementById('contact-form').reset();
+    document.getElementById("contact-form").reset();
 
-  // Show the popup message
-  document.getElementById('popup').style.display = 'flex';
-});
+    document.getElementById("popup").style.display = "flex";
+  });
 
-// Close the popup
 function closePopup() {
-  document.getElementById('popup').style.display = 'none';
+  document.getElementById("popup").style.display = "none";
 }
